aa59752c060c0f7cf69a0a1617c9a875b89f71da
Joun Hyder <joun88hyder@gmail.com>
2024-12-05 11:31:33 -0600

    updated part 1

--- part1-linear-regression/a6_part1.py
+++ part1-linear-regression/a6_part1.py
@@ -11,13 +11,19 @@ y = data["Blood Pressure"].values
 x = x.reshape(-1,1)
 
 # Create the model
-
+model = LinearRegression().fit(x, y)
 # Find the coefficient, bias, and r squared values. 
 # Each should be a float and rounded to two decimal places. 
+coef = round(float(model.coef_[0]), 2)
+intercept = round(float(model.intercept_), 2)
+r_squared = model.score(x, y)
 
-
+x_predict = 
+prediction = model.predict([[x_predict]])
 # Print out the linear equation and r squared value
-
+print(f"Model's Linear Equation: y = {coef}x + {intercept}")
+print(f"R Squared value: {r_squared}")
+print(f"Prediction is when x is {x_predict}: {prediction}")
 # Predict the the blood pressure of someone who is 43 years old.
 # Print out the prediction
 
