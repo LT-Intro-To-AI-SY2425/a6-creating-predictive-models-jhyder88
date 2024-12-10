0ceecc60e5c488e3dce5ea2038f7dbcd184e253c
Joun Hyder <joun88hyder@gmail.com>
2024-12-10 11:07:56 -0600

    updated part 2

--- part2-training-testing-data/a6_part2.py
+++ part2-training-testing-data/a6_part2.py
@@ -15,29 +15,36 @@ y = data["Blood Pressure"].values
 # Create your training and testing datasets:
 
 # Use reshape to turn the x values into 2D arrays:
-# xtrain = xtrain.reshape(-1,1)
+xtrain = xtrain.reshape(-1,1)
 
 # Create the model
-
+model = LinearRegression().fit(xtrain, ytrain)
 # Find the coefficient, bias, and r squared values. 
 # Each should be a float and rounded to two decimal places. 
-
-
+coef = round(float(model.coef[0]), 2)
+intercept = round(float(model.intercept_), 2)
+r_squared = model.score(xtrain, ytrain)
 # Print out the linear equation and r squared value:
+print("Model's Linear Equation: y=", coef, "x+", intercept)
+print("R Squared value:", r_squared)
 
 '''
 **********TEST THE MODEL**********
 '''
 # reshape the xtest data into a 2D array
-
+predict = model.predict(xtest)
 # get the predicted y values for the xtest values - returns an array of the results
 
 # round the value in the np array to 2 decimal places
-
+predict = np.around(predict, 2)
 
 # Test the model by looping through all of the values in the xtest dataset
 print("\nTesting Linear Model with Testing Data:")
-
+for index in range(len(xtest)):
+    actual = ytest[index]
+    predicted_y = predict[index]
+    x_coord = xtest[index] 
+    print("x value:", float(x_coord[0]), "Predicted y value:", predicted_y, "Actual y value:", actual)
 
 '''
 **********CREATE A VISUAL OF THE RESULTS**********
