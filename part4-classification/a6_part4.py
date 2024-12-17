7ac719b915f346c2f10b049f5fff2eccb1273d4f
Joun Hyder <joun88hyder@gmail.com>
2024-12-17 11:23:09 -0600

    finished part 4

--- part4-classification/a6_part4.py
+++ part4-classification/a6_part4.py
@@ -10,18 +10,46 @@ x = data[["Age", "EstimatedSalary", "Gender"]].values
 y = data["Purchased"].values
 
 # Step 1: Print the values for x and y
-
+print(data)
 # Step 2: Standardize the data using StandardScaler, 
-
+scaler = StandardScaler().fit(x)
+x = scaler.transform(x)
 # Step 3: Transform the data
-
+x = scaler.transform(x)
 # Step 4: Split the data into training and testing data
-
+x_train, x_test, y_train, y_test = train_test_split(x, y)
 # Step 5: Fit the data
-
+model = linear_model.LogisticRegression().fit(xTraining,yTraining)
 # Step 6: Create a LogsiticRegression object and fit the data
-
+model = linear_model.LogisticRegression().fit(x_train, y_train)
 # Step 7: Print the score to see the accuracy of the model
-
+print("Model Score:", model.score(xTest, yTest))
 # Step 8: Print out the actual ytest values and predicted y values
 # based on the xtest data
+for index in range(len(xTest)):
+    obs_x = xTest[index]
+    obs_x = obs_x.reshape(-1,3)
+    print("Personal Information:", obs_x)
+    
+    if model.predict(obs_x) == 1:
+        pred = True
+    else:
+        pred = False
+    if pred:
+        print("Prediction: Purchased")
+    else:
+        print("Prediction: Not Purchased")
+
+    if yTest[index] == 1:
+        acc = True
+    else:
+        acc = False
+    if acc:
+        print("Actual: Purchased")
+    else:
+        print("Actual: Not Purchased")
+    
+    if pred == acc:
+        print("The Model is Correct")
+    else:
+        print("The Model is not Correct")
